0fe19f13cca742b9d8147f6436d815590c600e84
Yevhenii <ke1vin.kapustian@gmail.com>
2025-07-02 20:45:40 +0200

    Changed path for images

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,19 +4,19 @@ export const products = new Map<string, ProductsType[]>([
     ['plans', [
         {
             name: "Gain muscles",
-            image: "/gainMuscles.png",
+            image: "/images/products/gainMuscles.png",
             description: "Build strength and size with targeted workouts and optimal nutrition.",
             price: 12,
         },
         {
             name: "Lose Weight",
-            image: "/loseWeight.png",
+            image: "/images/products/loseWeight.png",
             description: "Burn fat efficiently and reach your ideal shape with smart training and healthy habits.",
             price: 11
         },
         {
             name: "Nutrition Plans",
-            image: "/nutritionPlans.png",
+            image: "/images/products/nutritionPlans.png",
             description: "Fuel your body right with balanced, goal-focused meal plans tailored for results.",
             price: 9
         },
