b7fdb1b94f672bc37e44cdd51dfe17e9c5cab833
Yevhenii <ke1vin.kapustian@gmail.com>
2025-07-04 20:12:31 +0200

    header

--- src/app/motion-wrapper.tsx
+++ src/app/motion-wrapper.tsx
@@ -6,8 +6,8 @@ import { ReactNode } from "react";
 export default function LayoutWrapperMotion ({children}: {children:ReactNode }) {
     return(
         <motion.main
-                    initial={{opacity: 0, y: 40}}
-                    animate={{opacity: 1, y: 0}}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
                     transition={{duration: 0.5}}
                     className="flex-grow">
             {children}
