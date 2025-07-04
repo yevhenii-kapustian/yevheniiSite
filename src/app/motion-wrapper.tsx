95b2ddaab8a190e0940e24bbe855b8f4003eeb69
Yevhenii <ke1vin.kapustian@gmail.com>
2025-07-04 21:44:30 +0200

    Fixed problem with header

--- src/app/motion-wrapper.tsx
+++ src/app/motion-wrapper.tsx
@@ -6,8 +6,8 @@ import { ReactNode } from "react";
 export default function LayoutWrapperMotion ({children}: {children:ReactNode }) {
     return(
         <motion.main
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
+                    initial={{opacity: 0, y: 20}}
+                    animate={{opacity: 1, y: 0}}
                     transition={{duration: 0.5}}
                     className="flex-grow">
             {children}
--- src/components/WelcomeSection/animation.ts
+++ src/components/WelcomeSection/animation.ts
@@ -11,7 +11,7 @@ export const titleVariant = {
 
 export const descriptionVariant= {
     hidden: {
-        x: 100,
+        x: -100,
         opacity: 0,
     },
     visible: {
