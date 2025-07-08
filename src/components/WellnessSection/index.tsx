76d0f4c4aa7517e5a90e53f1124dae5adb983924
Yevhenii <ke1vin.kapustian@gmail.com>
2025-07-08 16:14:27 +0200

    Animation

--- src/components/WellnessSection/index.tsx
+++ src/components/WellnessSection/index.tsx
@@ -1,37 +1,65 @@
 import { babes } from "@/app/fonts";
 import Products from "../Products";
 import Link from "next/link";
-import { motion, useInView } from "framer-motion";
+import { easeInOut, motion, useInView } from "framer-motion";
 import { wellnessContainerStyles, 
          wellnessTextWrapperStyles, 
          wellnessButtonStyles,
          productContainer } from "./styles";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { productVisibility } from "./animation";
 
 const WellnessSection = () => {
 
     const ref = useRef(null)
     const isInView = useInView(ref, {once: true})
+    const [isMobileWidth, setIsMobileWidth] = useState<boolean>(false)
+
+    useEffect(() => {
+        const handleWidth = () => {
+            if (window.innerWidth > 1000) {
+                setIsMobileWidth(true)
+            } 
+        }
+
+        handleWidth()
+
+    })
+    
 
     return(
-        <section className={wellnessContainerStyles}>
-            <div className={wellnessTextWrapperStyles}>
-                <h2 className={`${babes.className} text-7xl uppercase max-[1201]:text-6xl max-md:text-6xl`}>Invest in Your Health Today</h2>
-                <p>
-                    Your body is your most valuable asset. Start building long-term strength, energy, and resilience — one healthy
-                    choice at a time. Small daily actions lead to powerful results. Make your health a priority, starting now.
-                </p>
-                <Link className={wellnessButtonStyles} href="/programs">Find Your Plan</Link>
-            </div>
+        <section className="relative overflow-hidden">
             <motion.div
-                ref={ref}
-                className={productContainer}
-                variants={productVisibility}
-                initial='hidden'
-                animate={isInView ? "visible" : "hidden"}
-                >
-                <Products showPrice={false} showBuy={false} showName={false}/>
+                    initial={{
+                            scale: 1,
+                            }}
+                    transition={{
+                                duration: 0.5,
+                                ease: easeInOut
+                                }}
+                    viewport={{
+                                amount: 0.5
+                            }}
+                    whileInView={isMobileWidth ? {scale: 1.05} : {scale: 1}}
+                    className={wellnessContainerStyles}>
+
+                <div className={wellnessTextWrapperStyles}>
+                    <h2 className={`${babes.className} text-7xl uppercase max-[1201]:text-6xl max-md:text-6xl`}>Invest in Your Health Today</h2>
+                    <p>
+                        Your body is your most valuable asset. Start building long-term strength, energy, and resilience — one healthy
+                        choice at a time. Small daily actions lead to powerful results. Make your health a priority, starting now.
+                    </p>
+                    <Link className={wellnessButtonStyles} href="/programs">Find Your Plan</Link>
+                </div>
+                <motion.div
+                    ref={ref}
+                    className={productContainer}
+                    variants={productVisibility}
+                    initial='hidden'
+                    animate={isInView ? "visible" : "hidden"}
+                    >
+                    <Products showPrice={false} showBuy={false} showName={false}/>
+                </motion.div>
             </motion.div>
         </section>
     )
