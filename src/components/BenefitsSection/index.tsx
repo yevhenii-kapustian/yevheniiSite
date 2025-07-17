762eae0bc377a2792376f923de8dfcb59f798261
Yevhenii <ke1vin.kapustian@gmail.com>
2025-07-17 23:56:30 +0200

    Added animation Benefits

--- src/components/BenefitsSection/index.tsx
+++ src/components/BenefitsSection/index.tsx
@@ -11,10 +11,20 @@ import { itemsContainerStyles,
          getLinkStyles} from "./styles"
 import Image from "next/image"
 import Link from "next/link"
+import { motion, useInView } from 'framer-motion'
+import { useRef } from "react"
 
 const BenefitsSection = () => {
+    const ref = useRef(null)
+    const isInView = useInView(ref, {once: true})
+
     return(
-        <section className="px-20 py-10 max-sm:px-5 flex flex-col items-center">
+        <motion.section
+                        ref={ref} 
+                        initial={{y: -100, opacity: 0}}
+                        animate={isInView ? {y: 0, opacity: 1} : {}}
+                        transition={{duration: 0.5}}
+                        className="px-20 py-10 max-sm:px-5 flex flex-col items-center">
             <h2 className={`${babes.className} ${mainTitleStyles}`}>How it works</h2>
             <div className={itemsContainerStyles}>
                 {benefitsSteps.map((item:BenefitsStepsType, index:number) => (
@@ -27,7 +37,7 @@ const BenefitsSection = () => {
                 ))}
             </div>
             <Link className={getLinkStyles} href="#">Get My Personalized Plan</Link>
-        </section>
+        </motion.section>
     )
 }
 
