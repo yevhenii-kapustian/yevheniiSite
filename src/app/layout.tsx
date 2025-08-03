9bf10ef5b73377af4765e7f1d76b7861142d3693
Yevhenii <ke1vin.kapustian@gmail.com>
2025-08-03 19:18:06 +0200

    Last one

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import CookieConsentBanner from "@/components/Cookie";
 import OrganizationSchema from "@/components/scripts/OrganizationSchema";
 import GoogleAnalytics from "@/components/scripts/GoogleAnalytics";
 import { siteMetadata } from "@/config/siteMetadata";
+import FacebookAppIdMeta from "@/components/FacebookAppIdMeta";
 
 
 const openSans = Open_Sans({
@@ -28,9 +29,9 @@ export default function RootLayout({
         <meta property="fb:app_id" content="1682995145732700" />
       </head>
       <body className={`${openSans.className} antialiased flex flex-col`}>
+        <FacebookAppIdMeta/>
         <GoogleAnalytics/>
         <OrganizationSchema/>
-
         <Header />
         <main className="flex-grow">
           <ProductsProvider>
---
+++ src/components/FacebookAppIdMeta.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function FacebookAppIdMeta() {
+  return (
+    <Head>
+      <meta property="fb:app_id" content="1682995145732700" />
+    </Head>
+  );
+}
