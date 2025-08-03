3bb05f961697910754b88d153658a3de523e9c34
Yevhenii <ke1vin.kapustian@gmail.com>
2025-08-03 13:04:03 +0200

    Added analitik

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ProductsProvider } from "@/context/ProductsContext";
 import CookieConsentBanner from "@/components/Cookie";
+import Script from "next/script";
 
 const openSans = Open_Sans({
   weight: ["400","500","600","700","800"],
@@ -12,7 +13,7 @@ const openSans = Open_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Yevhenii",
+  title: "Yevhenii Fit",
   description: "Transform your body and mind with our science-backed fitness programs tailored to your goals. Start today!",
 };
 
@@ -26,6 +27,19 @@ export default function RootLayout({
       <body
         className={`${openSans.className} antialiased flex flex-col`}
       >
+         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2ZT3WQLMKV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2ZT3WQLMKV');
+          `}
+        </Script>
+
         <Header/>
           <main className="flex-grow">
             <ProductsProvider>
