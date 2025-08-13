8e5e9b2bbbee5d7e53ea2368ca478e464fa7d47a
Yevhenii <ke1vin.kapustian@gmail.com>
2025-08-13 11:14:44 +0200

    env

--- src/components/Cookie/index.tsx
+++ src/components/Cookie/index.tsx
@@ -7,6 +7,9 @@ import { cookieContainer,
         } from "./styles";
 
 export default function CookieConsentBanner() {
+  console.log("FB Pixel ID:", process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID);
+  const fbPixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
+
   return (
     <>
       <CookieConsent
@@ -40,7 +43,7 @@ export default function CookieConsentBanner() {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+              fbq('init', '${fbPixelId}');
               fbq('track', 'PageView');
             }
           `,
